a435644c737a127653b8a9e242329cfe8735115e
JeongTaek <jtkim965@naver.com>
2024-11-13 15:48:04 +0900

    #2 Refactor : market컬럼 생성 및 데이터 주입

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pykrx import stock
 import mysql.connector
 
+jusicDate = "20240919"
 # MySQL 연결 설정
 connection = mysql.connector.connect(
     host="127.0.0.1",  # Docker 컨테이너가 로컬에서 실행 중이므로 localhost 사용
@@ -17,7 +18,8 @@ cursor.execute("SET NAMES utf8mb4;")  # 세션 인코딩 설정
 create_table_query = """
 CREATE TABLE IF NOT EXISTS stock_data (
     ticker VARCHAR(10) PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    market VARCHAR(10) NOT NULL
 ) CHARACTER SET utf8mb4;
 """
 cursor.execute(create_table_query)
@@ -27,17 +29,25 @@ cursor.execute("DELETE FROM stock_data")
 print("기존 데이터를 모두 삭제했습니다.")
 
 # 데이터를 삽입할 함수 정의
-def insert_stock_data(ticker, name):
+def insert_stock_data(ticker, name, market):
     try:
-        sql = "INSERT INTO stock_data (ticker, name) VALUES (%s, %s)"
-        cursor.execute(sql, (ticker, name))
+        sql = "INSERT INTO stock_data (ticker, name, market) VALUES (%s, %s, %s)"
+        cursor.execute(sql, (ticker, name, market))
     except mysql.connector.Error as err:
         print(f"Error: {err}")
 
 # KRX에서 데이터 가져와 MySQL에 삽입
-for ticker in stock.get_market_ticker_list("20241112", market="ALL"):
+for ticker in stock.get_market_ticker_list(jusicDate, market="KOSPI"):
     name = stock.get_market_ticker_name(ticker)
-    insert_stock_data(ticker, name)
+    insert_stock_data(ticker, name, "KOSPI")
+
+for ticker in stock.get_market_ticker_list(jusicDate, market="KOSDAQ"):
+    name = stock.get_market_ticker_name(ticker)
+    insert_stock_data(ticker, name, "KOSDAQ")
+
+for ticker in stock.get_market_ticker_list(jusicDate, market="KONEX"):
+    name = stock.get_market_ticker_name(ticker)
+    insert_stock_data(ticker, name, "KONEX")
 
 # 데이터베이스 변경 사항 커밋 및 연결 종료
 connection.commit()
