c7ae7cda4e39a18f8192736ace0d284700ac6980
JeongTaek <jtkim965@naver.com>
2024-11-26 13:26:47 +0900

    Feat : 한국투자증권 API 엑세스 토큰 생성 및 저장

---
+++ write_token.py
@@ -0,0 +1,44 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+# 환경변수에서 APP_KEY와 APP_SECRET 읽어오기
+APP_KEY = os.environ.get('APP_KEY')
+APP_SECRET = os.environ.get('APP_SECRET')
+
+# # API 요청 URL
+base_url = 'https://openapivts.koreainvestment.com:29443/oauth2/tokenP'
+
+# # 요청 본문 데이터
+body_data = {
+    'grant_type': 'client_credentials',
+    'appkey': APP_KEY,
+    'appsecret': APP_SECRET
+}
+
+# # 요청 헤더
+headers = {
+    'Content-Type': 'application/json',
+}
+
+# # API 호출
+try:
+    response = requests.post(base_url, json=body_data, headers=headers)
+    response.raise_for_status()  # 요청이 실패하면 예외 발생
+
+    data = response.json()
+
+    if 'access_token' in data:
+        access_token = data['access_token']
+        print(f"Access Token: {access_token}")
+
+        # 텍스트 파일에 토큰 저장
+        with open('./token.txt', 'w') as file:
+            file.write(access_token)
+
+    else:
+        print('토큰을 받을 수 없습니다.')
+
+except requests.exceptions.RequestException as error:
+    print(f"API 호출 중 오류 발생: {error}")
